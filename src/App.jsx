e0a484109326d42cd2a38ccaa1db8c74cfa41646
CFCode-git <435184766@qq.com>
2021-04-01 23:28:31 +0800

    精确渲染：对比新旧数据变化，只在自己的数据变化的时候 render

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,6 @@
-import React, {useState, useContext, useEffect} from 'react'
+import React from 'react'
+import {appContext,store,connect} from './redux.jsx'
 
-const store = {
-  state: {
-    user: {name: 'frank', age: 18}
-  },
-  setState(newState) {
-    store.state = newState
-    store.listeners.map(fn => fn(store.state))
-  },
-  listeners: [],
-  subscribe(fn) {
-    store.listeners.push(fn)
-    return () => {
-      const index = store.listeners.indexOf(fn)
-      store.listeners.splice(index, 1)
-    }
-  }
-}
-
-const connect = (Component) => {
-  return (props) => {
-    const {state, setState} = useContext(appContext)
-    const [, update] = useState({})
-    // 初次挂载 添加订阅 执行 setState(update)
-    useEffect(() => {
-      store.subscribe(() => {
-        update({})
-      })
-    }, [])
-    const dispatch = (action) => {
-      setState(reducer(state, action))
-    }
-    return <Component {...props} dispatch={dispatch} state={state}/>
-  }
-}
-
-const reducer = (state, {type, payload}) => {
-  if (type === 'updateUser') {
-    return {
-      ...state,
-      user: {
-        ...state.user,
-        ...payload
-      }
-    }
-  } else {
-    return state
-  }
-}
-
-const appContext = React.createContext(null)
 export const App = () => {
   return (
     <appContext.Provider value={store}>
@@ -70,17 +21,21 @@ const 二儿子 = () => {
     <UserModifier>aaa</UserModifier>
   </section>
 }
-const 幺儿子 = () =>{
+const 幺儿子 = connect(state=>{
+  return {group:state.group}
+})(({group}) =>{
   console.log('幺儿子执行了',Math.random())
-  return <section>幺儿子</section>
-}
-
-const User = connect(({state,dispatch}) => {
-  console.log('User 执行了', Math.random())
-  return <div>User:{state.user.name}</div>
+  return <section>幺儿子<div>Group: {group.name}</div></section>
 })
 
-const UserModifier = connect(({dispatch,state,children}) => {
+const User = connect(state=>{
+  return {user:state.user}
+})(({user}) => {
+  console.log('User 执行了', Math.random())
+  return <div>User:{user.name}</div>
+})
+
+const UserModifier = connect()(({dispatch,state,children}) => {
   console.log('UserModifier 执行了',Math.random())
   const onChange = (e) => {
     dispatch({type: 'updateUser', payload: {name: e.target.value}})
---
+++ src/redux.jsx
@@ -0,0 +1,71 @@
+import React, {useContext, useEffect, useState} from 'react'
+
+export const appContext = React.createContext(null)
+
+export const store = {
+  state: {
+    user: {name: 'frank', age: 18},
+    group: {name: '前端组'}
+  },
+  setState(newState) {
+    store.state = newState
+    store.listeners.map(fn => fn(store.state))
+  },
+  listeners: [],
+  subscribe(fn) {
+    store.listeners.push(fn)
+    return () => {
+      const index = store.listeners.indexOf(fn)
+      store.listeners.splice(index, 1)
+    }
+  }
+}
+const reducer = (state, {type, payload}) => {
+  if (type === 'updateUser') {
+    return {
+      ...state,
+      user: {
+        ...state.user,
+        ...payload
+      }
+    }
+  } else {
+    return state
+  }
+}
+
+const changed = (oldState, newState) => {
+  let changed = false
+  for (let key in oldState) {
+    if(oldState.hasOwnProperty(key)){
+      if (oldState[key] !== newState[key]) {
+        changed = true
+      }
+    }
+  }
+  return changed
+}
+
+export const connect = (selector) => (Component) => {
+  return (props) => {
+    const {state, setState} = useContext(appContext)
+    const [, update] = useState({})
+    const data = selector ? selector(state) : {state}
+    // 初次挂载 添加订阅 执行 setState(update)
+    useEffect(() => {
+      const unsubscribe = store.subscribe(() => {
+        // subscribe 里面的这个函数会在 setState 之后拿出来执行
+        const newData = selector ? selector(store.state) : {state: store.state}
+        if (changed(data, newData)) {
+          console.log('update')
+          update({})
+        }
+      })
+      return unsubscribe
+    }, [selector])
+    const dispatch = (action) => {
+      setState(reducer(state, action))
+    }
+    return <Component {...props} {...data} dispatch={dispatch}/>
+  }
+}
