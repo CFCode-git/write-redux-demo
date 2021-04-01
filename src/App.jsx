2eeb4cea8c3b1b8f06b0afca15e0c48f33a0d1bf
CFCode-git <435184766@qq.com>
2021-04-01 13:51:35 +0800

    存在问题：render 太多

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,21 @@ export const App = () => {
   )
 }
 
-const 大儿子 = () => <section>大儿子<User/></section>
-const 二儿子 = () => <section>二儿子<UserModifier>aaa</UserModifier></section>
-const 幺儿子 = () => <section>幺儿子</section>
+const 大儿子 = () => {
+  console.log('大儿子执行了',Math.random())
+  return <section>大儿子<User/></section>
+}
+const 二儿子 = () => {
+  console.log('二儿子执行了',Math.random())
+  return <section>二儿子<UserModifier>aaa</UserModifier></section>
+}
+const 幺儿子 = () =>{
+  console.log('幺儿子执行了',Math.random())
+  return <section>幺儿子</section>
+}
 
 const User = () => {
+  console.log('User 执行了',Math.random())
   const contextValue = useContext(appContext)
   return <div>User:{contextValue.appState.user.name}</div>
 }
@@ -52,6 +62,7 @@ const connect = (Component) => {
 
 
 const UserModifier = connect(({dispatch,state,children}) => {
+  console.log('UserModifier 执行了',Math.random())
   const onChange = (e) => {
     dispatch({type: 'updateUser', payload: {name: e.target.value}})
   }
